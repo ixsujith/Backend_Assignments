dd88678d57ff2834735815c7d79db78c433a10bd
Sujith <sujithchandraj09@gmail.com>
2026-03-05 11:46:31 +0530

    Spring Assignment 1

---
+++ 5th_March_2026_SpringAssignment_1/Task1/src/main/java/com/example/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class UserRepository {
+
+    public String userRepo1() {
+        String user1 = "User_1";
+        String domain = "Domain_1";
+        return user1 + ": " + domain;
+    }
+
+    public String userRepo2() {
+        String user2 = "User_2";
+        String domain = "Domain_2";
+        return user2 + ": " + domain;
+    }
+}
