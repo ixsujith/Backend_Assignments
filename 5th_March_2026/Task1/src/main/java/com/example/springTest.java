c2ce77ba38fd0e94270be336d3d7f017f2ba4b03
Sujith <sujithchandraj09@gmail.com>
2026-03-05 15:40:37 +0530

    Spring Assignment 2 Task 1

---
+++ 5th_March_2026/Task1/src/main/java/com/example/springTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class springTest {
+
+    public void output() {
+        System.out.println("Spring testing");
+    }
+}
